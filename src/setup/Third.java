00fdf1bf3e137a92e6ca3dad9a99da291e5ed737
Vitalii4Hrymalyuk <hrymalyuk2002@gamil.com>
2021-10-03 11:44:51 -0500

    Created third file

---
+++ src/setup/Third.java
@@ -0,0 +1,12 @@
+package setup;
+
+public class Third {
+
+    public void s() {
+        System.out.println("First method");
+    }
+
+    public void s(int num) {
+        System.out.println("Second method");
+    }
+}
